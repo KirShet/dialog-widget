4d86715d3fbdaf931f4b0952f2a0d2bc381ef63d
KirShet <kirshet2000@gmail.com>
2025-01-17 17:25:06 +0600

    Updated design

--- src/DialogWidget/DialogWidget.php
+++ src/DialogWidget/DialogWidget.php
@@ -59,10 +59,12 @@ class DialogWidget extends Widget
         $html .= '<div class="message spoilers-container" dir="auto">';
         $html .= '<h6 class="card-title">'.$p1.'</h6>';
         $html .= '<span>'.htmlspecialchars($message['text']).'</span>';
+        $html .= '<div>';
         $html .= '<span class="time-start d-inline-block text-muted"><span class="i18n" dir="auto">' . htmlspecialchars((string)$message['start']) . '</span>';
         $html .= '<div class="time-inner" title="Сообщение началось ' . htmlspecialchars((string)$message['start']) . '"><span class="i18n" dir="auto">' . htmlspecialchars((string)$message['start']) . '</span></div></span>';
         $html .= '<span class="time-end text-muted"><span class="i18n" dir="auto">' . htmlspecialchars((string)$message['end']) . '</span>';
         $html .= '<div class="time-inner" title="Сообщение закончилось ' . htmlspecialchars((string)$message['end']) . '"><span class="i18n" dir="auto">' . htmlspecialchars((string)$message['end']) . '</span></div></span>';
+        $html .= '</div>';
         $html .= '<span class="score">'.htmlspecialchars($message['score']).'</span></div>';
         $html .= '</div></div>';
         $html .= '<svg viewBox="-11 -4 29 29" class="bubble-tail"><use href="#message-tail-filled"></use></svg></div></div>';
